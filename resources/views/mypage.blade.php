ef7d2a563641786c6e0507ca5f356ba62f04619a
ckcic <ckcicic@gmail.com>
2022-11-21 10:04:04 +0900

    open post

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -18,24 +18,44 @@
                     주문 내역 확인
                 </div>
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <!-- {{ \App\Models\User::find(1)->post[0]['title'] }} -->
-                    <table>
-                        <th>제목</th>
-                        <th>작성자</th>
-                        <th>조회수</th>
-                        <th>작성일</th>
-                        <th>답변여부</th>
-                        
-                    @foreach (\App\Models\User::find(1)->post as $post)
-                    <tr>
-                        <td>{{ $post->title }}</td>
-                        <td>{{ $post->user_id }}</td>
-                        <td>{{ $post->hit }}</td>
-                        <td>{{ $post->created_at }}</td>
-                        <td>{{ $post->state? '답변 완료' : '답변 대기' }}</td>
-                    </tr>
-                    @endforeach
-                    </table>
+                    <div class="block" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+                        <div @click="open = ! open">
+                                게시글 내역 확인 
+                            </div>
+                        <div>
+                            <div x-show="open"
+                                    x-transition:enter="transition ease-out duration-200"
+                                    x-transition:enter-start="transform opacity-0 scale-95"
+                                    x-transition:enter-end="transform opacity-100 scale-100"
+                                    x-transition:leave="transition ease-in duration-75"
+                                    x-transition:leave-start="transform opacity-100 scale-100"
+                                    x-transition:leave-end="transform opacity-0 scale-95"
+                                    class="absolute z-50 mt-2 rounded-md shadow-lg "
+                                    style="display: none;"
+                                    @click="open = false">
+                                <div class="rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white">
+                                    총 게시글 수 {{ \App\Models\User::find(1)->post->count() }}
+                                    <table>
+                                        <th>제목</th>
+                                        <th>작성자</th>
+                                        <th>조회수</th>
+                                        <th>작성일</th>
+                                        <th>답변여부</th>
+        
+                                    @foreach (\App\Models\User::find(1)->post as $post)
+                                    <tr>
+                                        <td>{{ $post->title }}</td>
+                                        <td>{{ $post->user_id }}</td>
+                                        <td>{{ $post->hit }}</td>
+                                        <td>{{ $post->created_at }}</td>
+                                        <td>{{ $post->state? '답변 완료' : '답변 대기' }}</td>
+                                    </tr>
+                                    @endforeach
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
