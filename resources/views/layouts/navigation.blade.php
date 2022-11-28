2ef7751fdfc9c8702d613e2e3af4b663e6c959c3
ckcic <ckcicic@gmail.com>
2022-11-28 11:04:48 +0900

    gnb fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -31,16 +31,6 @@
                         {{ __('Q&A') }}
                     </x-nav-link>
                 </div>
-                <a href="{{ route('cart.list') }}" class="flex items-center ml-12 ">
-                    {{ __('장바구니') }}
-                    <svg class="w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path
-                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
-                        </path>
-                    </svg>
-                    <span>{{ Cart::getTotalQuantity() }}</span>
-                </a>
                 @auth
                 @if(Auth::user()->permission == 1)  
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
@@ -55,6 +45,18 @@
             @auth
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <div class="">
+                    <a href="{{ route('cart.list') }}" class="flex items-center ml-12 ">
+                        {{ __('장바구니') }}
+                        <svg class="w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path
+                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
+                            </path>
+                        </svg>
+                        <span>{{ Cart::getTotalQuantity() }}</span>
+                    </a>
+                </div>
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
@@ -90,6 +92,7 @@
                         @endauth         
                     </x-slot>
                 </x-dropdown>
+                
             </div>
             @else
             <!-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center py-4 sm:pt-0"> -->
