9d6149c51467572d62ca3d406e6065914088c825
Donggeun <ezfood3@naver.com>
2022-11-30 00:22:53 +0900

    customizing

--- resources/views/customizing.blade.php
+++ resources/views/customizing.blade.php
@@ -2,6 +2,11 @@
   <div class="py-6">
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div class="container flex">
+        <x-slot name="header">
+          <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('인형 커스터마이징') }}
+          </h2>
+        </x-slot>
         <img class="" src="https://gto.or.kr/images/img_page_ready.png" alt="image">
       </div>
     </div>
